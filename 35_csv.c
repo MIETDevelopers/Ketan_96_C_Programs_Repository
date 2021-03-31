ce2419472485b67f6d9fc9ad05f5ca04b7f42b8e
Ketan Abrol <2020a1r096@mietjammu.in>
2021-03-31 00:33:37 -0700

    c program

---
+++ 35_csv.c
@@ -0,0 +1,33 @@
+//Program which interacts with user and take student's name, roll number and attendance percentage and stores it in the csv file. 
+#include <stdio.h> //Including standard input output header files.
+#include <stdlib.h>//Including standard library header files.
+#include <string.h>//Including string header files. 
+int main() 
+{ 
+    FILE* fp = fopen("C:\\Users\\Lenovo\\Desktop\\program\\Ketan_96_C_Programs_Repository/attendance.csv", "a+"); //File path.
+
+    char name[50]; 
+    int rollno, attendance; 
+
+    if (!fp) { 
+        // Error in file opening 
+        printf("Can't open file\n"); 
+        return 0; 
+    }
+    // Asking user input for the new record to be added.   
+    printf("\nEnter Student Name\n"); 
+    scanf("%s", &name); 
+    printf("\nEnter Roll Number\n"); 
+    scanf("%d", &rollno); 
+    printf("\nEnter Attendance\n"); 
+    scanf("%d", &attendance); 
+
+    // Saving data in file 
+    fprintf(fp, "%s, %d, %d\n", name, 
+            rollno, attendance); 
+
+    printf("\nNew student details added."); 
+
+    fclose(fp); 
+    return 0; 
+}
