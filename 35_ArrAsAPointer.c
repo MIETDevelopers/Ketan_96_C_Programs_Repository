cbc7a048e7dab5792b8cc22ddd189d3c2cf4711c
Ketan Abrol <2020a1r096@mietjammu.in>
2021-03-30 23:21:05 -0700

    c program

---
+++ 35_ArrAsAPointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[10];
+    printf("enter the values of array");
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d",*(arr+i)); // array as a pointer
+    }
+}
